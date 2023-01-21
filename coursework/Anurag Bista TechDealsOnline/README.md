d443401b56effb2a44160f434370cb0a4fd0aa7e
AnuragBista <109964144+AnuragBista@users.noreply.github.com>
2023-01-21 23:28:09 +0545

    Add files via upload

---
+++ coursework/Anurag Bista TechDealsOnline/README.md
@@ -0,0 +1,1 @@
+# Anurag Bista online store
---
+++ coursework/Anurag Bista TechDealsOnline/aboutus.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>About TechDeals</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Amarante&family=Great+Vibes&display=swap" rel="stylesheet">
+
+  <style>
+  .jumbotron {
+    background-color: #f4cb1e;
+    color: #fff;
+    padding: 100px 25px;
+  }
+  .bg-grey {
+    background-color: #f6f6f6;
+  }
+  .container-fluid {
+    padding: 60px 50px;
+  }
+
+  .facebook{
+    background-color: #4267B2;
+  }
+  .instagram{
+    background-color: #3f729b;
+  }
+a{
+  text-decoration:none;
+    font-size: 40px;
+    font-family: 'Amarante';
+    color: rgb(76, 73, 82);
+}
+  a:hover{
+    text-decoration:none;
+    font-family: 'Amarante';
+    color: white;
+  }
+h1{
+    text-align: center;
+    font-size: 40px;
+    font-family: 'Amarante';
+    color: rgb(76, 73, 82);
+
+}
+h1 span{
+    font-family: 'Great Vibes';
+    font-weight: 100;
+}
+h1 i{
+    font-size: 25px;
+    color: rgb(152, 152, 139);
+}
+.div{
+  background:#b9b0ae;
+}
+.padding{
+  padding-left:15px;
+}
+  </style>
+</head>
+<body>
+<div class="jumbotron text-center">
+  <a href="homepage.php"><h1 class="techdeals">Tech<span>Deals</span>Online<i>.com</i></h1></a>
+  
+  <p>We specialize in techniques and technologies</p> 
+</div>
+
+<h2>What our customers say</h2>
+<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+  <div class="item active">
+      <h4>"Could I... BE any more happy with this company?"<br><span style="font-style:normal;">Micheal Chandler ,  Fighter, UFC</span></h4>
+    </div>
+    <div class="item">
+    <h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">Michael Jordan, Vice President, Charlotte Hornets</span></h4>
+    </div>
+    <div class="item">
+      <h4>"Buy the Stark Tech, don't Miss out buddy.!!"<br><span style="font-style:normal;">Tony Starks, CEO, Starks Industry</span></h4>
+    </div>
+    
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> 
+<hr>
+<!-- Container (About Section) -->
+<div class="container-fluid">
+<p>I'm Anurag or Annrag, the developer of this website.</p>      
+  <p>I got one motive here, to have a running and active website for my UNIVERSITY coursework. Lets see, if it works!!!.
+</p>
+<hr>
+<h2>Contact Tech<span>D</span>eals<i>.com</i></h2>
+<p>Email: thewebsiterdeveloper@gmail.com</p>
+<p>Contact Number: 9813890623</p>
+<h3>You can also connect with me in.. </h3>
+<a href="https://www.facebook.com/people/Annrag-Bist/100009710114803/" target="_blank" class="padding"><img src="images/fb.png"/></a>
+<a href="https://www.instagram.com/bistaanurag123/?hl=en" target="_blank" class="padding"><img src="images/insta.png"/></a>
+<a href="https://github.com/AnuragBista" target="_blank" class="padding"><img src="images/github.png"/></a>
+
+</br>
+</br></br>
+<input type="submit" class="btn btn-primary" onclick="history.back();" name="back" value="Back"/>
+</div>
+<hr>
+</body>
+</html>
